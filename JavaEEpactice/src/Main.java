525ab907b909b2dc7d9585f4739139d2649c39ed
shamsragimova <rahimova070@gmail.com>
2024-01-19 14:18:13 +0400

    Shams modified

--- JavaEEpactice/src/Main.java
+++ JavaEEpactice/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world, Azar!");
         System.out.println("Hello Firuddin");
-        System.out.println("Hello, Aze :) ..... Shams.");
+        System.out.println("Hello, Azer :) ..... Shams.");
     }
 }
